e8ed994b03355f9f5554c424664e0a8e9071994e
TwoPii <genisb8@rocketmail.com>
2025-01-07 17:07:36 +0100

    Added styled-components configuration and theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,28 @@
 import type { AppProps } from 'next/app';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
+const GlobalStyle = createGlobalStyle`
+html, body {
+    margin: 0;
+    padding: 0;
+}
+
+* {
+    box-sizing: border-box;
+}`;
+
+const theme = {
+    colors: {
+        primary: '#0070f3'
+    }
+};
 export default function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <GlobalStyle />
+            <ThemeProvider theme={theme}>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </>
+    );
 }
